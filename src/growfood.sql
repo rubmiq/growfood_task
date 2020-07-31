3415818ee93dd3acb876756015d7e49f1272e2c9
rubmiq <rubmiq@bk.ru>
2020-07-31 05:19:41 +0400

    SQL commit

---
+++ src/growfood.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июл 31 2020 г., 01:18
+-- Версия сервера: 5.7.24
+-- Версия PHP: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `growfood`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `clients_phone_unique` (`phone`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `phone`, `address`) VALUES
+(1, 'asdasdasd', 'asdasdas', 'dasddasdasd'),
+(2, 'adsdas', 'asd', 'asd'),
+(3, 'sadasdasd', 'asdasdasdasd', 'asdasdasdasdas'),
+(4, 'зжцжзцзж', 'цзжцзж', 'цзжцзж');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2020_07_30_183731_orders', 1),
+(2, '2020_07_30_183755_clients', 1),
+(3, '2020_07_30_184216_tariffs', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `tariff_id` int(11) NOT NULL,
+  `start` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `client_id`, `tariff_id`, `start`) VALUES
+(1, 1, 2, '2020-07-25'),
+(2, 2, 2, '2020-07-22'),
+(3, 3, 2, '2020-07-10'),
+(4, 4, 2, '2020-07-16');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tariffs`
+--
+
+DROP TABLE IF EXISTS `tariffs`;
+CREATE TABLE IF NOT EXISTS `tariffs` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `weekdays` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `tariffs`
+--
+
+INSERT INTO `tariffs` (`id`, `name`, `price`, `weekdays`) VALUES
+(1, 'test1', 5000, '[\"on\",\"on\",\"on\",\"on\",\"on\",\"on\",\"on\"]'),
+(2, 'test1', 4200, '[\"on\",\"on\",\"off\",\"on\",\"on\",\"on\",\"on\"]'),
+(3, 'test1', 3000, '[\"on\",\"on\",\"off\",\"on\",\"on\",\"off\",\"on\"]');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
